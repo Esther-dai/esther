6840194b77e70274b3488f3040e0e23affdbc32f
Esther <sp062177@gamil.com>
2021-03-25 19:59:47 +0800

    tab bar item&navigation controller try

---
+++ esther/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  esther
+//
+//  Created by Esther Dai on 2021/3/13.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
